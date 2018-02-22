dc017675ad315ab6eb8eca4d5c9c1ceeffffde2f
Luis Gustavo Grubert Valensuela <valensuela1@gmail.com>
2018-02-22 13:36:49 -0500

    Create homework2.java

---
+++ fau/assignment_2/homework2.java
@@ -0,0 +1,31 @@
+/**
+ * /****************************************************************************
+ * Name: Luis Gustavo Grubert Valensuela Z#:23351882 lvalensuela2015@fau.edu
+ * Course: JavaProgramming
+ * Professor: Dr. Mehrdad Nojoumian
+ * Due Date:02/22/2018             Due Time: 11:30PM
+ * Assignment Number: 02
+ * Last Changed: 02/22/2018
+ *
+ * Description:
+ * In this assignment we will check the return value of the statement:
+ * System.out.println("\"\tTest\\\\\rIt\'");
+ * that is It'
+/*******************************************************************************
+ */
+package homework_2;
+
+/**
+ *
+ * @author valen
+ */
+public class Homework_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("\"\tTest\\\\\rIt\'");
+    }
+    
+}
