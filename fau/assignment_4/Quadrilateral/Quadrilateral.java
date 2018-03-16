6f8edd7bd0208315141925f611b227da1d1e38ca
Luis Gustavo Grubert Valensuela <valensuela1@gmail.com>
2018-03-15 20:32:22 -0400

    Update Quadrilateral.java

--- fau/assignment_4/Quadrilateral/Quadrilateral.java
+++ fau/assignment_4/Quadrilateral/Quadrilateral.java
@@ -1,7 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * /****************************************************************************
+ * Name: Luis Gustavo Grubert Valensuela Z#:23351882 lvalensuela2015@fau.edu
+ * Course: JavaProgramming
+ * Professor: Dr. Mehrdad Nojoumian
+ * Due Date:03/15/2018             Due Time: 11:30PM
+ * Assignment Number: Assignment 4
+ * Last Changed: 03/15/2018
+ *
+ * Description:
+ * class called Quadrilateral that can be used to represent a quadrilateral.
+/*******************************************************************************
  */
 package assignment4;
 
@@ -13,68 +21,90 @@ public class Quadrilateral
     private Point p3;
     private Point p4;
   
-    public Quadrilateral()
-    {
+    public Quadrilateral(){
+        /**
+         * Default Constructor
+         */
         p1 = new Point(1, 1);
         p2 = new Point(2, 1);
         p3 = new Point(2, 2);
         p4 = new Point(1, 2);
     }
 
-    public Quadrilateral(Point newP1, Point newP2, Point newP3, Point newP4)
-    {
+    public Quadrilateral(Point newP1, Point newP2, Point newP3, Point newP4){
+        /**
+         * Default Constructor taking four Points as arguments
+         */
         p1 = new Point(newP1.getX(), newP1.getY());
         p2 = new Point(newP2.getX(), newP2.getY());
         p3 = new Point(newP3.getX(), newP3.getY());
         p4 = new Point(newP4.getX(), newP4.getY());
     }
   
-    public void setPoint1(Point newP)
-    {
+    public void setPoint1(Point newP){
+        /**
+         * Method to set Point 1
+         */
        p1.setX(newP.getX());
        p1.setY(newP.getY());
     }
   
-    public void setPoint2(Point newP)
-    {
+    public void setPoint2(Point newP)    {
+        /**
+         * Method to set Point 2
+         */
        p2.setX(newP.getX());
        p2.setY(newP.getY());
     }
   
-    public void setPoint3(Point newP)
-    {
+    public void setPoint3(Point newP)    {
+       /**
+        * Method to set Point 3
+        */
        p3.setX(newP.getX());
        p3.setY(newP.getY());
     }
   
-    public void setPoint4(Point newP)
-    {
+    public void setPoint4(Point newP)    {
+       /**
+        * Method to set Point 1
+        */
        p4.setX(newP.getX());
        p4.setY(newP.getY());
     }  
   
-    public Point getPoint1()
-    {
+    public Point getPoint1()    {
+       /**
+        * Method to return Point 1
+        */
        return p1;
     }
   
-    public Point getPoint2()
-    {
+    public Point getPoint2()    {
+        /**
+        * Method to return Point 2
+        */
        return p2;
     }
   
-    public Point getPoint3()
-    {
+    public Point getPoint3()    {
+        /**
+        * Method to return Point 3
+        */
        return p3;
     }
   
-    public Point getPoint4()
-    {
+    public Point getPoint4()    {
+        /**
+        * Method to return Point 4
+        */
        return p4;
     }
   
-    public Point getPoint(int pointNum)
-    {
+    public Point getPoint(int pointNum)    {
+        /**
+        * Method to return Point pointNum
+        */
         switch(pointNum)
         {
             case 1:
@@ -90,8 +120,10 @@ public class Quadrilateral
         }
     }
   
-    public void setPoint(int pointNum, Point newP)
-    {
+    public void setPoint(int pointNum, Point newP)    {
+        /**
+        * Method to set Point pointNum to newP
+        */
         switch(pointNum)
         {
             case 1:
@@ -109,8 +141,10 @@ public class Quadrilateral
         }
     }
 
-    public boolean isParallelogram()
-    {      
+    public boolean isParallelogram()    {      
+       /**
+        * Method to test if the 4 points form a Parallelogram
+        */
        double d12 = Math.pow(p2.getX() - p1.getX(), 2.0) + Math.pow(p2.getY() - p1.getY(), 2.0);
        double d23 = Math.pow(p3.getX() - p2.getX(), 2.0) + Math.pow(p3.getY() - p2.getY(), 2.0);
        double d34 = Math.pow(p4.getX() - p3.getX(), 2.0) + Math.pow(p4.getY() - p3.getY(), 2.0);
@@ -130,8 +164,10 @@ public class Quadrilateral
        return true;
     }
 
-    public boolean isRectangle()
-    {
+    public boolean isRectangle()    {
+        /**
+        * Method to test if the 4 points form a Rectangle
+        */
        if(isParallelogram())
        {
            double d13 = Math.pow(p3.getX() - p1.getX(), 2.0) + Math.pow(p3.getY() - p1.getY(), 2.0);
@@ -146,8 +182,10 @@ public class Quadrilateral
            return false;
     }
 
-    public boolean isSquare()
-    {
+    public boolean isSquare()    {
+        /**
+        * Method to test if the 4 points form a Square
+        */
        if(isRectangle())
         {
            double d12 = Math.pow(p2.getX() - p1.getX(), 2.0) + Math.pow(p2.getY() - p1.getY(), 2.0);
@@ -162,8 +200,10 @@ public class Quadrilateral
            return false;      
     }
   
-    public String toString()
-    {
+    public String toString()    {
+        /**
+        * Method to print the four points
+        */
         return "Point1: " + p1 + "\n" + "Point2: " + p2 + "\n" + "Point3: " + p3 + "\n" + "Point4: " + p4;
     }  
 
