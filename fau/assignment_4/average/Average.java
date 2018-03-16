4c5f63b3e34c1faca4467b4db34aa83a219cb4a2
Luis Gustavo Grubert Valensuela <valensuela1@gmail.com>
2018-03-15 20:15:21 -0400

    Update Average.java

--- fau/assignment_4/average/Average.java
+++ fau/assignment_4/average/Average.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * /****************************************************************************
+ * Name: Luis Gustavo Grubert Valensuela Z#:23351882 lvalensuela2015@fau.edu
+ * Course: JavaProgramming
+ * Professor: Dr. Mehrdad Nojoumian
+ * Due Date:03/15/2018             Due Time: 11:30PM
+ * Assignment Number: Assignment 4
+ * Last Changed: 03/15/2018
+ *
+ * Description:
+ * class called Average that can be used to calculate average of several 
+ * integers. It should contain the following methods:
+/*******************************************************************************
  */
 package assigment4;
 
 import java.util.*;
 public class Average {
-
+    /*Class Average that calculates the average of several integers*/
     public double returnAverage(int...numbers) {
+        /*Method to return the average value of n numbers*/
         int total = 0;
         for(int i = 0; i < numbers.length; i++) {
             total += numbers[i];
@@ -16,19 +26,14 @@ public class Average {
             return (double)total/numbers.length;
     }
    
-    public int returnRange(int firstElement, int secondElement) {
-        if(firstElement < secondElement)
-        {
-            return -1;
-        }
-        else
-        {
-            return firstElement - secondElement;
-        }
-        
-    }
-        
-    public double returnRange2(int...numbers) {
+    
+    public double returnRange(int...numbers) {
+        /*Method to return the average of two integers that represent a range
+        method that accepts two integer parameters that represent a range. 
+        Issue an error message and return zero if the second parameter is less 
+        than the first one. Otherwise, the method should return the average of
+        the integers in that range (inclusive).
+         */
         int holder = 0;
 
         int highest;
@@ -66,18 +71,13 @@ public class Average {
         Average myAvg = new Average();
         System.out.println(myAvg.returnAverage(2,4));
         System.out.println(myAvg.returnAverage(2,4,5));
+
+        System.out.println("---------------------------------------------------");
         
-        /*
-        System.out.println(myAvg.returnRange(5, 1));
-        System.out.println(myAvg.returnRange(6, 5));
-        System.out.println(myAvg.returnRange(4, 5));
-        */
-        System.out.println("++");
-        
-        System.out.println(myAvg.returnRange2(7,15));
-        System.out.println(myAvg.returnRange2());
-        System.out.println(myAvg.returnRange2(21,15));
-        System.out.println(myAvg.returnRange2(27,1));
+        System.out.println(myAvg.returnRange(7,15));
+        System.out.println(myAvg.returnRange());
+        System.out.println(myAvg.returnRange(21,15));
+        System.out.println(myAvg.returnRange(27,1));
     }
     
 }
