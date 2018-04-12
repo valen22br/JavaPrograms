b60535747287a28ef39b875d544476d97b7e6db4
Luis Gustavo Grubert Valensuela <valensuela1@gmail.com>
2018-04-12 14:57:58 -0400

    Create Car.java

---
+++ fau/lab_10/Q2/Car.java
@@ -0,0 +1,23 @@
+/*
+ * Name: Luis Gustavo Grubert Valensuela Z#:23351882 lvalensuela2015@fau.edu
+ * Course: JavaProgramming
+ * Professor: Dr. Mehrdad Nojoumian
+ * Due Date:04/12/2018             Due Time: 11:30PM
+ * Assignment Number: lab 08
+ * Last Changed: 03/29/2018
+ *
+ * Description:
+ * Program to show the creation of an interface
+ */
+package lab10.q2;
+
+/**
+ * Interface Car
+ * @author valen
+ */
+public interface Car {
+    void changeGear(int a); 
+    void speedUp(int a); 
+    void applyBrakes(int a);
+    
+}
